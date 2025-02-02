9121911b9d1db0be0ba732c1c4238b1c665a5f01
Yar Kravtsov <yarlson@gmail.com>
2025-02-02 17:24:13 +0200

    Brew formula update for duh version 0.4.0

--- duh.rb
+++ duh.rb
@@ -5,21 +5,21 @@
 class Duh < Formula
   desc "duh - a minimalist Docker UI that doesn't get in your way."
   homepage "https://github.com/yarlson/duh"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/yarlson/duh/releases/download/0.3.0/duh_0.3.0_darwin_amd64.tar.gz"
-      sha256 "67d496c50f4a8fb6defd7f811025936f369ace279d4bc433d936e36f11bbc66f"
+      url "https://github.com/yarlson/duh/releases/download/0.4.0/duh_0.4.0_darwin_amd64.tar.gz"
+      sha256 "caa5338a4a863d76c91209c4b742a1707b9f73cdfe0c30db000eaebef3451bf2"
 
       def install
         bin.install "duh"
       end
     end
     on_arm do
-      url "https://github.com/yarlson/duh/releases/download/0.3.0/duh_0.3.0_darwin_arm64.tar.gz"
-      sha256 "26c7b82dd6cdb65745f0ff3f6e940a8c13fdcf08d64e361cf2f0910341825df1"
+      url "https://github.com/yarlson/duh/releases/download/0.4.0/duh_0.4.0_darwin_arm64.tar.gz"
+      sha256 "7b3d2a825091aaa8e0de25460447f67d3d76d0a5fee118cbd18382b8590e7cae"
 
       def install
         bin.install "duh"
@@ -30,8 +30,8 @@ class Duh < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/yarlson/duh/releases/download/0.3.0/duh_0.3.0_linux_amd64.tar.gz"
-        sha256 "51ff387263eca72d753b6ecba502201e6896140a44df40d97585ff2a337ab740"
+        url "https://github.com/yarlson/duh/releases/download/0.4.0/duh_0.4.0_linux_amd64.tar.gz"
+        sha256 "8b02c5640b7e7dc0caebb351df0bef26c16ebce00c437190b28909e7eab64031"
 
         def install
           bin.install "duh"
@@ -40,8 +40,8 @@ class Duh < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/yarlson/duh/releases/download/0.3.0/duh_0.3.0_linux_arm64.tar.gz"
-        sha256 "a33faddc6319d5addd35281d85bbb202906614babd7a12e13df961af37ace907"
+        url "https://github.com/yarlson/duh/releases/download/0.4.0/duh_0.4.0_linux_arm64.tar.gz"
+        sha256 "4fb3bb50df790830915dba5dd43cf72e8f84aeb02315f3e3ae2344bc37c8cd85"
 
         def install
           bin.install "duh"
